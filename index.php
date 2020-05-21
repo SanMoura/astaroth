6c6278364837f798a51878e1cd33a9bd562e6638
Marlus Santiago <santiago.marlus@gmail.com>
2020-05-20 23:05:29 -0300

    site front

---
+++ index.php
@@ -0,0 +1,98 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v4.0.1">
+    <title>Blog Template · Bootstrap</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/blog/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://getbootstrap.com/docs/4.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <!-- Favicons -->
+<link rel="apple-touch-icon" href="/docs/4.5/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+<link rel="icon" href="/docs/4.5/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+<link rel="icon" href="/docs/4.5/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+<link rel="manifest" href="/docs/4.5/assets/img/favicons/manifest.json">
+<link rel="mask-icon" href="/docs/4.5/assets/img/favicons/safari-pinned-tab.svg" color="#563d7c">
+<link rel="icon" href="/docs/4.5/assets/img/favicons/favicon.ico">
+<meta name="msapplication-config" content="/docs/4.5/assets/img/favicons/browserconfig.xml">
+<meta name="theme-color" content="#563d7c">
+
+
+    <style>
+      body{
+        width: 100%;
+        height: 100%;
+        background-image: url('assets/img/bg.png');
+        background-position: center center;
+        background-repeat: no-repeat;
+        background-attachment:fixed;
+        background-size: cover;
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="blog.css" rel="stylesheet">
+  </head>
+  <body >
+    <div class="container" style="background-color:white;">
+  <header class="blog-header py-3">
+    <div class="row flex-nowrap justify-content-between align-items-center">
+      <div class="col-12 text-center">
+        <img src="assets/img/logoAst.png" width="300em">
+      </div>
+    </div>
+  </header>
+
+  <div class="nav-scroller py-1 mb-2">
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light" style="text-transform: uppercase">
+<div style="margin-left: 50px;">
+    <a class="navbar-brand" href="#">Home</a>
+    <a class="navbar-brand" href="#">Cadastro</a>
+    <a class="navbar-brand" href="#">Download</a>
+    <a class="navbar-brand" href="#">Tutoriais</a>
+    <a class="navbar-brand" href="#">Ranking</a>
+    <a class="navbar-brand" href="#">Doação</a>
+    <a class="navbar-brand" href="#">DropList</a>
+    <a class="navbar-brand" href="#">Suporte</a>
+    <a class="navbar-brand" href="#">DISCORD</a>
+    </div>  
+</nav>
+  </div>
+
+  <div class="text-white rounded bg-dark text-center">
+      <!-- <img src="assets/img/astaroth.png" > -->
+      <iframe width="1100" height="540" src="https://www.youtube.com/embed/scE_pZ8O2ps?rel=0&modestbranding=1&showinfo=0&controls=0&autoplay=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </div>
+<br/>
+  <div class="row mb-2">
+    <div class="col-md-6">
+      <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <h1 class="text-center"><strong class="d-inline-block mb-2 text-primary">CADASTRO</strong></h1>
+        </div>
+ 
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <h1 class="text-center"><strong class="d-inline-block mb-2 text-success"><a href="http://www.mediafire.com/file/ibdz63cr95tbeiu/Instalador+Astaroth.exe">DOWNLOAD</a></strong></h1>
+
+        </div>
+     
+      </div>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
